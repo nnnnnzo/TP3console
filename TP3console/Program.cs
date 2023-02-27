98a42058758c9fe5546471ebcbf72f519ac51150
nnnnnzo <lagadecenzo@gmail.com>
2023-02-27 11:04:06 +0100

    finre

--- TP3console/Program.cs
+++ TP3console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Castle.Components.DictionaryAdapter;
 using Microsoft.EntityFrameworkCore;
+using System.IO.Pipes;
 using System.Linq;
 using TP3console.Models.EntityFramework;
 
@@ -138,6 +139,79 @@ namespace TP3console
 
             }
             Console.WriteLine(user);
+        }
+
+        public static void AddUser()
+        {
+            var ctx = new FilmsDBContext();
+            Utilisateur moi = new Utilisateur();
+            moi.Login = "lagadece";
+            moi.Email = "email1@test.fr";
+            moi.Pwd= "password";
+
+            ctx.Utilisateurs.Add(moi);
+            ctx.SaveChanges();
+        }
+
+        public static void Edit() {
+            var ctx = new FilmsDBContext();
+
+            var films = ctx.Films.First(film => film.Nom.ToLower() == "l'armee des douze singes");
+            films.Description = "desc test";
+            ctx.SaveChanges();
+
+        }
+
+        public static void Delete()
+        {
+            var ctx = new FilmsDBContext();
+            var films = ctx.Films.First(film => film.Nom.ToLower() == "l'armee des douze singes");
+
+            foreach (var avis in ctx.Avis.Where(s => s.Film == films.Id))
+            {
+                ctx.Avis.Remove(avis);
+            }
+            ctx.Films.Remove(films);
+            ctx.SaveChanges();
+        }
+
+        public static void AddAvis() { 
+            var ctx = new FilmsDBContext();
+            var films = ctx.Films.First(film => film.Nom.ToLower() == "blade runner");
+            Avi avi = new Avi();
+            var userMoi = ctx.Utilisateurs.First(user => user.Login.ToLower() == "lagadece");
+            avi.Utilisateur = userMoi.Id;
+            avi.Film = films.Id;
+            avi.Avis = "Masterclass";
+            avi.Note = (decimal)0.99;
+            avi.UtilisateurNavigation = userMoi;
+            avi.FilmNavigation = films;
+
+            ctx.Avis.Add(avi);
+            ctx.SaveChanges();
+
+        }
+
+        public static void AddRng() { 
+            var ctx = new FilmsDBContext();
+            var catDrame = ctx.Categories.First(cat => cat.Nom.ToLower() == "drame");
+
+
+            Film film1 = new Film();
+            Film film2 = new Film();
+
+            film1.Nom = "Bullet Train";
+            film1.Description = "Cinq tueurs à gages se retrouvent dans un train à grande vitesse voyageant entre Tokyo et Morioka et ne faisant que très peu d'arrêts. Les cinq criminels vont découvrir qu'ils sont tous liés par leur mission, se demandent qui va en sortir vivant et s'interrogent sur ce qui les attend à la gare d'arrivée.";
+            film1.Categorie = catDrame.Id;
+            film1.CategorieNavigation = catDrame;
+
+            film2.Nom = "The Batman";
+            film2.Description = "Dans sa deuxième année de lutte contre le crime, le milliardaire et justicier masqué Batman explore la corruption qui sévit à Gotham et notamment comment elle pourrait être liée à sa propre famille, les Wayne, à qui il doit toute sa fortune. En parallèle, il enquête sur les meurtres d'un tueur en série qui se fait connaître sous le nom de Sphinx et sème des énigmes cruelles sur son passage.";
+            film2.Categorie = catDrame.Id;
+            film2.CategorieNavigation = catDrame;
+
+            ctx.Films.AddRange(film1,film2);
+            ctx.SaveChanges();
         }
 
 
